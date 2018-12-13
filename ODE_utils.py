fd7a8106088358a3e6924d2a928a23b02d76acda
bpiotrzkowski <37160952+bpiotrzkowski@users.noreply.github.com>
2018-12-13 12:33:28 -0600

    Add files via upload

--- ODE_utils.py
+++ ODE_utils.py
@@ -29,46 +29,29 @@ def rc(M,Q,Lambda):
         rcosm=np.inf
     return rcosm
 
-def boundaryv(scal,ubdytype,bdytype,Nv,ru0,dr0v,dv0,vmax,M0,Q,Lambda,scalarfield,A,rcosmtol,datatype):
-    
-    
-    if datatype==object:
-        rnpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        signpv=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        phinpv=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        drnpv=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        dsignpv=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        dphinpv=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        drnpu=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        dsignpu=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        dphinpu=np.zeros((Nv*scal),dtype=datatype)*Decimal(0)
-        scald=Decimal(scal)
-        dt=dv0/(scald)
-    else:
-        rnpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        signpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        phinpv=np.zeros((Nv*scal),dtype=datatype)#*np.nan
-        drnpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        dsignpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        dphinpv=np.zeros((Nv*scal),dtype=datatype)#*np.nan
-        drnpu=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        dsignpu=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        dphinpu=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        massnpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
-        scalf=float(scal)
-        dt=dv0/(scalf)
-    
-
+def boundaryv(scal,ubdytype,bdytype,Nv,ru0,dr0v,dv0,vmax,M0,Q,Lambda,scalarfield,A,rcosmtol,scalarfieldtype,power):
+    datatype=float
+    rnpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    signpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    phinpv=np.zeros((Nv*scal),dtype=datatype)#*np.nan
+    drnpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    dsignpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    dphinpv=np.zeros((Nv*scal),dtype=datatype)#*np.nan
+    drnpu=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    dsignpu=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    dphinpu=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    massnpv=np.zeros((Nv*scal),dtype=datatype)*np.nan
+    scalf=float(scal)
+    dt=dv0/(scalf)
     
     rnpv[0]=ru0 
     drnpv[0]=dr0v
-    #Nv=Nv*scal
-   
-    
+
     if scalarfield==True:
         #A=.115
         #A=.2
-        scalarfieldtype='pulse'#'power'
+        
+        #scalarfieldtype='pulse'#'power'
         if scalarfieldtype=='pulse':
             v1=1.0
             v2=5.0
@@ -85,7 +68,7 @@ def boundaryv(scal,ubdytype,bdytype,Nv,ru0,dr0v,dv0,vmax,M0,Q,Lambda,scalarfield
         if scalarfieldtype=='power':
             v1=1
             v2=vmax
-            power=-1
+            #power=-1
             v1n=int(v1*(Nv*scal)/vmax)
             v2n=int(v2*(Nv*scal)/vmax)
 
@@ -151,11 +134,6 @@ def boundaryv(scal,ubdytype,bdytype,Nv,ru0,dr0v,dv0,vmax,M0,Q,Lambda,scalarfield
             
     #######
     elif bdytype=="edd":
-        
-        #signpv[0]=mth.log(1.0-2.0*M0/ru0+Q**2.0/ru0**2.0)
-        
-        #dsignpv[0]=0.0
-        #drnpv[0]=dr0v
         
         signpv[0]=np.log(1.0-2.0*M0/ru0+Q**2.0/ru0**2.0-Lambda*ru0**2/3)
         
@@ -243,7 +221,117 @@ def boundaryv(scal,ubdytype,bdytype,Nv,ru0,dr0v,dv0,vmax,M0,Q,Lambda,scalarfield
                 #print(rcosm)
             else:
                 break
+  
+    ############################
+    ############################
+                
+    elif bdytype=="EH":
+        beta=.1
+        #power=3
+        #power=-power
+        print(power)
+        rplus=M0+(M0**2.0-Q**2.0)**(.5)
+        v0=((rplus-ru0)/beta)**(-1/power)
+        
+        signpv=np.zeros((Nv*scal),dtype=datatype)
+        dsignpv=np.zeros((Nv*scal),dtype=datatype)
+        
+        
+        
+        
+        drnpv[0]=beta*power*(v0)**(-power-1) 
+        #dsignpv[0]=-(power+1)/v0#kappaq(ru0,Q)*0.0
+        rnpv[0]=ru0
+        phinpv[0]=0.0
+        
+        dphinpv[0]=(beta*power*(power+1)*(v0)**(-power-2)/(rplus-beta*(v0)**(-power)))**(.5)
+        print('dr/dv is '+str(drnpv[0]))
+        #sigv0=0.0
+        
+        dsignpv[0]=kappaq(ru0,Q)#np.abs(Rfunc(drnpv[0],drnpu[0],rnpv[0],signpv[0],Q,Lambda)/drnpu[0])#-(power+1)/v0#kappaq(ru0,Q)*0.0
+        signpv[0]=kappaq(ru0,Q)*v0#np.log(.5*np.abs(drnpu[0]))
+        
+        M=Mq(.90,Q)
+        print(0.0,0.0,M)
+        #signpv[0]=.1#np.log(.5*np.abs(drnpv[0]))
+        drnpu[0]=-1/(4*drnpv[0])*np.exp(signpv[0])*(1-2*M/rnpv[0]+(Q/rnpv[0])**2-Lambda*rnpv[0]**2/3)
+        
+        
+        
+        print(0.0,0.0,drnpu[0],drnpv[0])
+        
+        dphinpu[0]=0.0
+        #dphinpv[0]=0.0
+        if ubdytype=="stan":
+            dsignpu[0]=0.0
+        elif ubdytype=="adapt":
+            dsignpu[0]=-(-1/2*np.log(2)-np.log(dr0v)+3/2*np.log(2))
+        elif ubdytype=="edd":
+            dsignpu[0]=2*(3*Q**2-3*M0*ru0+Lambda*ru0**4)/(ru0*(-3*Q**2+ru0*(6*M0-3*ru0+Lambda*ru0**3)))*drnpu[0]
+            print("Using Full Eddington Coordinates")
+        #signpv[0]=0.0
+        massnpv[0]=M0
+        v=v0
+        
+        rcosm=rc(massnpv[0],Q,Lambda)
+        for j in range(0,Nv*scal-1):
+            
+            #print(rcosm)
+            if rnpv[j]+dt*drnpv[j]>0.0 and rnpv[j]+dt*drnpv[j]<rcosm-rcosmtol:
+                ###Predictor###
+                v=v+dt
+                
+                rnpv[j+1]=rnpv[j]+dt*drnpv[j]
+                drnpv[j+1]=beta*power*(v)**(-power-1)
+                drnpu[j+1]=drnpu[j]+dt*Rfunc(drnpv[j],drnpu[j],rnpv[j],signpv[j],Q,Lambda)
+                
+                dphinpv[j+1]=((beta*power*(power+1)*(v)**(-power-2)+dsignpv[j+1]*beta*power*(v)**(-power-1))/(rplus-beta*(v)**(-power)))**(.5)
+                phinpv[j+1]=phinpv[j]+dt*dphinpv[j]
+                
+                dsignpv[j+1]=kappaq(rnpv[j+1],Q)
+                #signpv[j+1]=kappaq(ru0,Q)*v0
+                #dsignpv[j+1]=-(power+1)/v#kappaq(rnpv[j+1],Q)*0.0
+                signpv[j+1]=signpv[j]+dt*dsignpv[j]
+                
+                dphinpu[j+1]=dphinpu[j]+dt*Phifunc(drnpv[j],drnpu[j],dphinpu[j],dphinpv[j],rnpv[j]) 
+                dsignpu[j+1]=dsignpu[j]+dt*Sigfunc(drnpv[j],drnpu[j],dphinpu[j],dphinpv[j],rnpv[j],signpv[j],Q) 
+                ###
+                
+                #dsignpv[j+1]=0.0
+                #signpv[j+1]=0.0
        
+                #massnpv[j+1]=(1+4.0*drnpu[j+1]*drnpv[j+1]*np.exp(-signpv[j+1]))*rnpv[j+1]/2.0+Q**2.0/(2*rnpv[j+1])-Lambda*rnpv[j+1]**3.0/6.0
+
+                
+                ###Corrector###           
+                #signpv[j+1]=sigv0
+                rnpv[j+1]=rnpv[j]+0.5*dt*(drnpv[j]+drnpv[j+1])
+                #drnpv[j+1]=beta*power*(v)**(-power-1)
+                drnpu[j+1]=drnpu[j]+0.5*dt*(Rfunc(drnpv[j],drnpu[j],rnpv[j],signpv[j],Q,Lambda)+Rfunc(drnpv[j+1],drnpu[j+1],rnpv[j+1],signpv[j+1],Q,Lambda))
+                
+                dsignpv[j+1]=kappaq(rnpv[j+1],Q)
+                #signpv[j+1]=kappaq(ru0,Q)*v0
+                #dsignpv[j+1]=-(power+1)/v#kappaq(rnpv[j+1],Q)*0.0
+                signpv[j+1]=signpv[j]+0.5*dt*(dsignpv[j]+dsignpv[j+1])
+                
+                phinpv[j+1]=phinpv[j]+0.5*dt*(dphinpv[j]+dphinpv[j+1])
+                
+                dphinpu[j+1]=dphinpu[j]+0.5*dt*(Phifunc(drnpv[j],drnpu[j],dphinpu[j],dphinpv[j],rnpv[j])+Phifunc(drnpv[j+1],drnpu[j+1],dphinpu[j+1],dphinpv[j+1],rnpv[j+1])) 
+                dsignpu[j+1]=dsignpu[j]+0.5*dt*(Sigfunc(drnpv[j],drnpu[j],dphinpu[j],dphinpv[j],rnpv[j],signpv[j],Q)+Sigfunc(drnpv[j+1],drnpu[j+1],dphinpu[j+1],dphinpv[j+1],rnpv[j+1],signpv[j+1],Q)) 
+                massnpv[j+1]=(1+4.0*drnpu[j+1]*drnpv[j+1]*np.exp(-signpv[j+1]))*rnpv[j+1]/2.0+Q**2.0/(2*rnpv[j+1])-Lambda*rnpv[j+1]**3.0/6.0
+                
+                
+                #signpv[j+1]=np.log(1.0-2.0*massnpv[j+1]/rnpv[j+1]+Q**2.0/rnpv[j+1]**2.0-Lambda*rnpv[j+1]**2/3)
+                
+                #massnpv[j+1]=(1+4.0*drnpu[j+1]*drnpv[j+1]*np.exp(-signpv[j+1]))*rnpv[j+1]/2.0+Q**2.0/(2*rnpv[j+1])-Lambda*rnpv[j+1]**3.0/6.0
+               
+                
+                #dsignpv[j+1]=2*((massnpv[j+1]*rnpv[j+1]-Q**2-1/3*Lambda*rnpv[j+1]**4)/(rnpv[j+1]**3-2*massnpv[j+1]*rnpv[j+1]**2+Q**2*rnpv[j+1]-Lambda/3*rnpv[j+1]**5))*drnpv[j+1]
+                
+                rcosm=rc(massnpv[j+1],Q,Lambda)
+                #print(rcosm)
+            else:
+                break   
         
         
     #print(Nv)      
@@ -326,4 +414,10 @@ def massparam(x,y):
     result,result2=optimize.curve_fit(func,x,y)
     
     return result
-    
+
+def kappaq(rplus,Q):
+    M=(Q**2+rplus**2)/(2*rplus)
+    return (M**2-Q**2+M*(M**2-Q**2)**(.5))/(M+(M**2-Q**2)**(.5))**3
+
+def Mq(rplus,Q):
+    return (Q**2+rplus**2)/(2*rplus)
